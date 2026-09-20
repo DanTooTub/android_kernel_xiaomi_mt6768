f4f4f11b062d6a2148f7db488e410de6eba59821
DanTooTub <savely22827@yandex.ru>
2026-09-20 20:49:07 +0300

    Implement KSU manual hooks in exec.c
    Added hooks for KSU manual execution handling.

--- fs/exec.c
+++ fs/exec.c
@@ -1894,12 +1894,23 @@ out_ret:
 		putname(filename);
 	return retval;
 }
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+			       void *argv, void *envp, int *flags);
+__attribute__((hot))
+extern int ksu_handle_post_execveat(int *fd, struct filename **filename_ptr,
+				    void *argv, void *envp, int *flags, int *retval);
+#endif
 
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
